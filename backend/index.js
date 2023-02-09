401447c3b6fce16dbeada075a0c72e0fa4327bef
Cristian Infante <71674005+Cristian-Infante@users.noreply.github.com>
2023-02-08 22:01:18 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,29 +6,11 @@ import Ruta from "./Schemas/rutaSchema.js";
 import Horario from "./Schemas/horarioSchema.js";
 const app = express();
 
-
-import mongoose from 'mongoose';
-
-const stringConnection = 'mongodb+srv://CFIC:160004518@cluster0.zj56lft.mongodb.net/UNIRUTAS';
-const conectar =         'mongodb://127.0.0.1:27017/UNIRUTAS';
-//const conectar = 'mongodb://localhost:27017';
-
-mongoose.set("strictQuery", false);
-mongoose.connect(conectar)
-  .then(() => {
-    console.log("Conectado con exito a base de datos\n");
-  })
-  .catch((error) => {
-    console.log("Error al conectar a la base de datos \n", error)
-  })
-export default mongoose;
-
-
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hola Mundo");
+  res.send("HELLO WORLD!");
 })
 
 app.get("/getIP", async (req, res) => {
@@ -113,5 +95,5 @@ app.get("/delRuta/:nombre", async (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("\nConexion establecida en el puerto 5000");
+  console.log("\nCONNECTION ESTABLISHED AT PORT 5000");
 });
