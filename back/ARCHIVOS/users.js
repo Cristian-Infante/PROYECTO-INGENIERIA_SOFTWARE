ec2874724bc53b814a2d4a6886a1bba58ef8c136
Cristian Infante <71674005+Cristian-Infante@users.noreply.github.com>
2023-02-08 20:20:39 -0500

    Add files via upload

---
+++ back/ARCHIVOS/users.js
@@ -0,0 +1,19 @@
+export const users = [{
+    id: "160004518",
+    name: "CRISTIAN INFANTE",
+    pass: "123",
+    rol: "ADM"
+  },
+  {
+    id: "160004529",
+    name: "LUIS PEREZ",
+    pass: "123",
+    rol: "EST"
+  },
+  {
+    id: "160004523",
+    name: "EMMANUEL MARTINEZ",
+    pass: "123",
+    rol: "EST"
+  }
+];
