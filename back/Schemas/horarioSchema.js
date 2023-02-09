765bfc3d6838afa1397e8a7a6496cb77bd4eb988
Cristian Infante <71674005+Cristian-Infante@users.noreply.github.com>
2023-02-08 20:21:26 -0500

    Delete back directory

--- back/Schemas/horarioSchema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const horarioSchema = new mongoose.Schema({
-    _id: String,
-    hora: String,
-});
-
-const Horario = mongoose.model("horarios", horarioSchema);
-export default Horario;
--- back/Schemas/rutaSchema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const rutaSchema = new mongoose.Schema({
-    _id: String,
-    nombre: String,
-});
-
-const Ruta = mongoose.model("rutas", rutaSchema);
-export default Ruta;
